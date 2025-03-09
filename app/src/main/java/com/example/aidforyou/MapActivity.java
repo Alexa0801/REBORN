bea61b836b4f1f6044410d8493949c9d33326539
Alexandra <alexandra_stanca08@yahoo.com>
2025-03-09 15:27:05 +0200

    final map

--- app/src/main/java/com/example/aidforyou/MapActivity.java
+++ app/src/main/java/com/example/aidforyou/MapActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -11,17 +13,22 @@ import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -35,11 +42,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.search.SearchBar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.checkerframework.checker.units.qual.A;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap googleMap;
@@ -50,24 +67,49 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private ListView entertainmentListView;
     private ListView healthListView;
     private ArrayAdapter<String> arrayAdapter;
+    private ArrayAdapter<String> institutionsArrayAdapter;
+    private ArrayAdapter<String> entertainmentArrayAdapter;
+    private ArrayAdapter<String> healthArrayAdapter;
+    private AlertDialog.Builder alert;
+
     private final String[] categories = {
             "Institutions", "Shops", "Restaurants",
             "Pharmacy and other health related", "Entertainment"
     };
+
+    private final ArrayList<String> institutions = new ArrayList<>();
+    private final ArrayList<String> entertainment = new ArrayList<>();
+    private final ArrayList<String> health = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        alert = new AlertDialog.Builder(this);
+
+
         listView = findViewById(R.id.listView);
         institutionsListView = findViewById(R.id.institutionsListView);
         entertainmentListView = findViewById(R.id.entertainmentListView);
         healthListView = findViewById(R.id.healthListView);
+
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_customtext, categories);
         listView.setAdapter(arrayAdapter);
+
         searchBar = findViewById(R.id.searchBar);
 
+        getInstitutions();
+        getEntertainment();
+        getHealth();
+
+        institutionsArrayAdapter = new ArrayAdapter<>(this, R.layout.list_customtext, institutions);
+        entertainmentArrayAdapter = new ArrayAdapter<>(this, R.layout.list_customtext, entertainment);
+        healthArrayAdapter = new ArrayAdapter<>(this, R.layout.list_customtext, health);
+
+        institutionsListView.setAdapter(institutionsArrayAdapter);
+        entertainmentListView.setAdapter(entertainmentArrayAdapter);
+        healthListView.setAdapter(healthArrayAdapter);
 
         searchBar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +117,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 listView.setVisibility(View.VISIBLE);
             }
         });
+
         listView.setOnItemClickListener((parent, view, position, id) -> {
             String selectedItem = (String) parent.getItemAtPosition(position);
             handleInput(selectedItem);
@@ -89,6 +132,67 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         checkLocationPermission();
     }
 
+    private void getInstitutions(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Institutions");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren())
+                {
+                    String name = (String) ds.getKey();
+                    institutions.add(name);
+                }
+                institutionsArrayAdapter.notifyDataSetChanged();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getEntertainment(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Entertainment");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren())
+                {
+                    String name = (String) ds.getKey();
+                    entertainment.add(name);
+                }
+                entertainmentArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getHealth(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Pharmacy&other");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren())
+                {
+                    String name = (String) ds.getKey();
+                    health.add(name);
+                }
+                healthArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     private void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -100,12 +204,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         switch (selectedItem){
             case "Institutions":
                 Toast.makeText(this.getApplicationContext(), "Selected: Institutions", Toast.LENGTH_SHORT).show();
+                Log.e("Institutions", institutions.toString());
+                institutionsClicked();
                 break;
             case "Entertainment":
                 Toast.makeText(this.getApplicationContext(), "Selected: Entertainment", Toast.LENGTH_SHORT).show();
+                entertainmentClicked();
                 break;
             case "Pharmacy and other health related":
                 Toast.makeText(this.getApplicationContext(), "Selected: PPharmacy and other health related" , Toast.LENGTH_SHORT).show();
+                healthClicked();
                 break;
             case "Shops":
                 Toast.makeText(this.getApplicationContext(), "Selected: Shops, no data for now", Toast.LENGTH_SHORT).show();
@@ -115,6 +223,143 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 break;
 
         }
+    }
+
+    private void institutionsClicked() {
+        institutionsListView.setVisibility(View.VISIBLE);
+        institutionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedInstitution = institutions.get(position);
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Institutions");
+
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot ds: snapshot.getChildren())
+                        {
+                            if(Objects.equals(ds.getKey(), selectedInstitution)){
+                                double latitude = (double) ds.child("latitude").getValue();
+                                double longitude = (double) ds.child("longitude").getValue();
+                                LatLng latLng = new LatLng(latitude, longitude);
+                                googleMap.clear();
+                                currentLocation();
+                                googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).position(latLng));
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+                                handleOpenMaps(selectedInstitution, latLng);
+                            }
+
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                Toast.makeText(MapActivity.this, "Clicked: " + selectedInstitution, Toast.LENGTH_SHORT).show();
+                institutionsListView.setVisibility(View.GONE);
+            }
+        });
+    }
+
+
+
+    private void entertainmentClicked(){
+        entertainmentListView.setVisibility(View.VISIBLE);
+        entertainmentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedEntertainment = entertainment.get(position);
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Entertainment");
+
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot ds: snapshot.getChildren())
+                        {
+                            if(Objects.equals(ds.getKey(), selectedEntertainment)){
+                                double latitude = (double) ds.child("latitude").getValue();
+                                double longitude = (double) ds.child("longitude").getValue();
+                                LatLng latLng = new LatLng(latitude, longitude);
+                                googleMap.clear();
+                                currentLocation();
+                                googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).position(latLng));
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+                                handleOpenMaps(selectedEntertainment, latLng);
+                            }
+
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                Toast.makeText(MapActivity.this, "Clicked: " + selectedEntertainment, Toast.LENGTH_SHORT).show();
+                entertainmentListView.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void healthClicked(){
+        healthListView.setVisibility(View.VISIBLE);
+        healthListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedHealth = health.get(position);
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Pharmacy&other");
+
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot ds: snapshot.getChildren())
+                        {
+                            if(Objects.equals(ds.getKey(), selectedHealth)){
+                                double latitude = (double) ds.child("latitude").getValue();
+                                double longitude = (double) ds.child("longitude").getValue();
+                                LatLng latLng = new LatLng(latitude, longitude);
+                                googleMap.clear();
+                                currentLocation();
+                                googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).position(latLng));
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+                                handleOpenMaps(selectedHealth, latLng);
+                            }
+
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                Toast.makeText(MapActivity.this, "Clicked: " + selectedHealth, Toast.LENGTH_SHORT).show();
+                healthListView.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void handleOpenMaps(String selectedItem, LatLng latLng){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                alert.setTitle(selectedItem)
+                        .setMessage("If you want to go to " + selectedItem + " press yes")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Uri gmmIntentUri = Uri.parse("geo:" + latLng.latitude + "," + latLng.longitude + "?q=" + latLng.latitude + "," + latLng.longitude);
+                                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                mapIntent.setPackage("com.google.android.apps.maps");
+
+                                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                                    startActivity(mapIntent);
+                                }
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .create().show();
+            }
+        }, 1500);
     }
 
     @Override
@@ -133,6 +378,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             return;
         }
 
+        currentLocationWithZoom();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void currentLocationWithZoom(){
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
             if (location != null) {
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
@@ -144,6 +394,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         });
     }
 
+    @SuppressLint("MissingPermission")
+    private void currentLocation(){
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.addMarker(new MarkerOptions()
+                        .position(userLocation)
+                        .icon(setIcon(this, R.drawable.outline_emoji_people_24)));
+
+            }
+        });
+    }
     private void searchLocation(String location) {
         Geocoder geocoder = new Geocoder(this);
         try {
